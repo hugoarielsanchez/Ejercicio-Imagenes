5b19744446debef08965f012af7c24f6c7938b63
Hugo <hugoarielsanchez8@gmail.com>
2021-04-29 14:53:20 -0300

    Agrego archivos

---
+++ script.js
@@ -0,0 +1,34 @@
+const btnAgregarCaja = document.getElementById("btnAgregar");
+const contenedor = document.getElementById("contenedor");
+const input = document.getElementById("texto");
+const inputImagen = document.getElementById("Imagen");
+const divGrande = document.getElementById("vistaGrande");
+const imgGrande = document.getElementById("imgGrande");
+const btnCerrar = document.getElementById("cerrarImagenGrande");
+
+btnAgregarCaja.addEventListener("click", function(){
+    const cajaNueva = document.createElement('div');
+    cajaNueva.classList.add('caja');
+
+    const nuevaImg = document.createElement('img');
+    nuevaImg.classList.add('img');
+    nuevaImg.src = inputImagen.value
+
+    nuevaImg.addEventListener('click', function(){
+        imgGrande.src = nuevaImg.src
+        divGrande.classList.add('mostrarDiv');
+    })
+
+    const nuevoP = document.createElement('p');
+    nuevoP.classList.add('titulo');
+    nuevoP.textContent = input.value
+
+    cajaNueva.appendChild(nuevaImg);
+    cajaNueva.appendChild(nuevoP);
+    contenedor.appendChild(cajaNueva);
+})
+
+btnCerrar.addEventListener("click", function() {
+    divGrande.classList.remove('mostrarDiv');
+})
+
